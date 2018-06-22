bc885073c7a339dc7bafa700c03f6789f3c49e38
guoqi <1395772260@qq.com>
2018-06-22 11:33:25 +0800

    增加简单滤镜

---
+++ app/src/main/java/com/guoqi/beautycamera/FilterListAdapter.java
@@ -0,0 +1,78 @@
+package com.guoqi.beautycamera;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FilterListAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<String> list;
+    private int checkItemPosition = 0;
+
+    public void setCheckItem(int position) {
+        checkItemPosition = position;
+        notifyDataSetChanged();
+    }
+
+    public FilterListAdapter(Context context, List<String> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    public void resetData(List<String> list) {
+        this.list = list;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (convertView != null) {
+            viewHolder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = LayoutInflater.from(context).inflate(R.layout.adapter_list_filter, null);
+            viewHolder = new ViewHolder();
+            viewHolder.mText = (TextView) convertView.findViewById(R.id.text);
+            convertView.setTag(viewHolder);
+        }
+        fillValue(position, viewHolder);
+        return convertView;
+    }
+
+    private void fillValue(int position, ViewHolder viewHolder) {
+        viewHolder.mText.setText(list.get(position));
+        if (checkItemPosition != -1) {
+            if (checkItemPosition == position) {
+                viewHolder.mText.setTextColor(context.getResources().getColor(R.color.colorAccent));
+            } else {
+                viewHolder.mText.setTextColor(Color.GRAY);
+            }
+        }
+    }
+
+    static class ViewHolder {
+        TextView mText;
+    }
+}
--- app/src/main/java/com/guoqi/beautycamera/ImgPreviewUI.kt
+++ app/src/main/java/com/guoqi/beautycamera/ImgPreviewUI.kt
@@ -4,9 +4,7 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import jp.co.cyberagent.android.gpuimage.GPUImage
-import jp.co.cyberagent.android.gpuimage.GPUImageFilter
-import jp.co.cyberagent.android.gpuimage.GPUImageGrayscaleFilter
+import jp.co.cyberagent.android.gpuimage.*
 import kotlinx.android.synthetic.main.ui_img_preview.*
 import java.io.IOException
 import java.io.InputStream
@@ -18,7 +16,8 @@ class ImgPreviewUI : AppCompatActivity() {
 
     private var gpuImage: GPUImage? = null
     var bitmap: Bitmap? = null
-    var isOriImg = true//是否是原图
+    private lateinit var filterListAdapter: FilterListAdapter
+    private var filterList = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,13 +25,8 @@ class ImgPreviewUI : AppCompatActivity() {
 
         initUI()
 
-        tv_ori.setOnClickListener {
-            isOriImg = true
-            setOri(bitmap)
-        }
-        tv_black.setOnClickListener {
-            setFilter(bitmap)
-        }
+        initData()
+
     }
 
     private fun initUI() {
@@ -54,16 +48,51 @@ class ImgPreviewUI : AppCompatActivity() {
         gpuImage?.setImage(bitmap)
     }
 
-    private fun setFilter(bitmap: Bitmap?) {
-        isOriImg = false
-        gpuImage?.setImage(bitmap)
-        gpuImage?.setFilter(GPUImageGrayscaleFilter())
+    private fun initData() {
+        btn_ori.setOnClickListener {
+            gpuImage?.setFilter(GPUImageFilter())
+            filterListAdapter.setCheckItem(-1)
+        }
+        filterList.add("黑白")
+        filterList.add("亮度")
+        filterList.add("曝光")
+        filterList.add("对比度")
+        filterList.add("饱和度")
+        filterList.add("反色")
+        filterList.add("色阶")
+        filterList.add("色调")
+        filterListAdapter = FilterListAdapter(this, filterList)
+        hlv_filter.adapter = filterListAdapter
+        hlv_filter.setOnItemClickListener { _, _, i, _ ->
+            filterListAdapter.setCheckItem(i)
+            when (filterList[i]) {
+                "黑白" -> {
+                    gpuImage?.setFilter(GPUImageGrayscaleFilter())
+                }
+                "亮度" -> {
+                    gpuImage?.setFilter(GPUImageBrightnessFilter(0.8f))
+                }
+                "曝光" -> {
+                    gpuImage?.setFilter(GPUImageExposureFilter(1.5f))
+                }
+                "对比度" -> {
+                    gpuImage?.setFilter(GPUImageContrastFilter(2.0f))
+                }
+                "饱和度" -> {
+                    gpuImage?.setFilter(GPUImageSaturationFilter(2.0f))
+                }
+                "反色" -> {
+                    gpuImage?.setFilter(GPUImageColorInvertFilter())
+                }
+                "色阶" -> {
+                    gpuImage?.setFilter(GPUImageSepiaFilter(1.2f))
+                }
+                "色调" -> {
+                    gpuImage?.setFilter(GPUImageToneCurveFilter())
+                }
+            }
+        }
     }
 
-    private fun setOri(bitmap: Bitmap?) {
-        isOriImg = true
-        gpuImage?.setImage(bitmap)
-        gpuImage?.setFilter(GPUImageFilter())
-    }
 
 }
